2aea5cf871e88d87db00d77bee9e49026f56586c
강소현 <conut1119@gmail.com>
2025-08-19 18:23:59 +0900

    [Feat] 일일퀘스트 구현 및 스크립터블 오브젝트 생성

---
+++ Assets/09_KSH_Folder/Scripts/DailyQuest.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "DailyQuest", menuName = "DailyQuest", order = 1)]
+public class DailyQuest : ScriptableObject
+{
+    [Header("퀘스트 이름")]
+    public string questName;
+    
+    [Header("퀘스트 목표")]
+    public QuestType questType;
+    public int questGoal;
+    
+    [Header("완료 테스트")]
+    public int currentProgress = 0;
+    [HideInInspector] public bool isComplete = false;
+}
+
+public enum QuestType
+{
+    ChallengeDungeon,
+    UseFood,
+    GetArtifact,
+    UseStemina,
+    LevelUp,
+    SkillLevelUp,
+    ChargeStemina
+}
