3ee19698fb4176b8aca7600e9471679d98a136d3
강소현 <conut1119@gmail.com>
2025-08-25 14:37:03 +0900

    [Feat, Fix] 뽑기 애니메이션 구현 및 버그 수정
    -아래에서 위로 부드럽게 올라가게 함.
    -10연차시 애니메이션 후 정렬되지 않은 오류 발생 및 수정

--- Assets/09_KSH_Folder/Scripts/Gacha/GachaResultUI.cs
+++ Assets/09_KSH_Folder/Scripts/Gacha/GachaResultUI.cs
@@ -1,11 +1,15 @@
-using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GachaResultUI : MonoBehaviour
 {
     [SerializeField] private Transform content;
     [SerializeField] private GachaUI gachaPrefab;
+    [SerializeField] private int x;
+    [SerializeField] private int y;
+    [SerializeField] private ScrollRect scrollRect;
 
     public void Show(List<CharacterData> characterDatas)
     {
@@ -13,11 +17,36 @@ public class GachaResultUI : MonoBehaviour
         {
             Destroy(child.gameObject);
         }
+        
+        scrollRect.verticalNormalizedPosition = 1f; //스크롤 위치 초기화
+        
+        List<RectTransform> spawnedRects = new List<RectTransform>();
 
-        foreach (var c in characterDatas)
+        float startY = -1000;
+        float spacing = 250;          
+
+        for (int i = 0; i < characterDatas.Count; i++)
         {
             var gacha = Instantiate(gachaPrefab, content);
-            gacha.SetData(c);
+            gacha.SetData(characterDatas[i]);
+
+            RectTransform rect = gacha.GetComponent<RectTransform>();
+            spawnedRects.Add(rect);
+            
+            if( i == 0)
+                rect.anchoredPosition = new Vector2(x, startY);
+            else    
+                rect.anchoredPosition = new Vector2(x, startY * i);
+        }
+        
+        for (int i = 0; i < spawnedRects.Count; i++)
+        {
+            RectTransform rect = spawnedRects[i];
+            Vector2 finalPos = new Vector2(x, y - i * spacing);
+
+            rect.DOAnchorPos(finalPos, 0.5f)
+                .SetEase(Ease.OutBack,0.8f)
+                .SetDelay(0.2f * i);
         }
     }
 }
