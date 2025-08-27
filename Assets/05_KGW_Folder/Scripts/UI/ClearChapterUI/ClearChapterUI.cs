c2c21f21a623700a2792644ee8ce4310352d788b
KimGiWoon <rldns20704@naver.com>
2025-08-27 10:41:30 +0900

    [Chore] File Rollback
    파일 롤백

---
+++ Assets/05_KGW_Folder/Scripts/UI/ClearChapterUI/ClearChapterUI.cs
@@ -0,0 +1,48 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+// 챕터 클리어 UI
+public class ClearChapterUI : MonoBehaviour //BaseUI
+{
+    [Header("Battle Manager Reference")]
+    [SerializeField] BattleUIManager _battleUIManager;
+
+    TMP_Text _expText;  // 겅험치 텍스트
+    TMP_Text _growthPointText;  // 성장 포인트 텍스트
+    Button _confirmButton;    // 로비 이동 버튼
+
+    // 컴포넌트의 할당
+    private void Awake()
+    {
+        //_expText = _panelContainer.GetComponentInChildren<TMP_Text>();
+        //_growthPointText = _panelContainer.GetComponentInChildren<TMP_Text>();
+        //_confirmButton = _panelContainer.GetComponentInChildren<Button>();
+        //_confirmButton.onClick.AddListener(LobbyButtonClick);
+
+        _expText = GetComponentInChildren<TMP_Text>();
+        _growthPointText = GetComponentInChildren<TMP_Text>();
+        _confirmButton = GetComponentInChildren<Button>();
+        _confirmButton.onClick.AddListener(LobbyButtonClick);
+    }
+
+    private void Start()
+    {
+        //// 획득한 경험치와 점수 출력
+        //_expText.text = ;
+        //_growthPointText.text = ;
+    }
+
+    // 로비 이동 버튼 클릭
+    private void LobbyButtonClick()
+    {
+        // TODO : 김기운 : 추후에 마이씬 매니저 교체 예정
+        SceneManager.LoadScene("KGW_TestLobbyScene");
+
+        // 해당 UI 비활성화
+        //_panelContainer.SetActive(false);
+
+        _battleUIManager._clearChapterUI.SetActive(false);
+    }
+}
