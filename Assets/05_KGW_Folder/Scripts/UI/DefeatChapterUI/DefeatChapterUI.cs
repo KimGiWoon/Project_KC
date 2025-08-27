ac727b9224c9e534de2976c4d8a6ac4a126cd1f8
KimGiWoon <rldns20704@naver.com>
2025-08-27 11:14:45 +0900

    [Chore] File Overwrite
    충돌 파일 덮어씌우기

--- Assets/05_KGW_Folder/Scripts/UI/DefeatChapterUI/DefeatChapterUI.cs
+++ Assets/05_KGW_Folder/Scripts/UI/DefeatChapterUI/DefeatChapterUI.cs
@@ -1,48 +1,40 @@
+using System;
 using TMPro;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using SDW;
 
 // 클리어 실패 UI
-public class DefeatChapterUI : MonoBehaviour //BaseUI
+public class DefeatChapterUI : BaseUI
 {
     [Header("Battle Manager Reference")]
-    [SerializeField] BattleUIManager _battleUIManager;
+    // private BattleManager _battleManager;
+    private TMP_Text _expText; // 겅험치 텍스트
+    private TMP_Text _growthPointText; // 성장 포인트 텍스트
+    private Button _confirmButton; // 로비 이동 버튼
 
-    TMP_Text _expText;  // 겅험치 텍스트
-    TMP_Text _growthPointText;  // 성장 포인트 텍스트
-    Button _confirmButton;    // 로비 이동 버튼
+    public Action<UIName> OnUICloseRequested;
 
     // 컴포넌트의 할당
     private void Awake()
     {
-        //_expText = _panelContainer.GetComponentInChildren<TMP_Text>();
-        //_growthPointText = _panelContainer.GetComponentInChildren<TMP_Text>();
-        //_confirmButton = _panelContainer.GetComponentInChildren<Button>();
-        //_confirmButton.onClick.AddListener(LobbyButtonClick);
-
-        _expText = GetComponentInChildren<TMP_Text>();
-        _growthPointText = GetComponentInChildren<TMP_Text>();
-        _confirmButton = GetComponentInChildren<Button>();
+        _panelContainer.SetActive(false); // 패널 비활성화
+        _expText = _panelContainer.GetComponentInChildren<TMP_Text>();
+        _growthPointText = _panelContainer.GetComponentInChildren<TMP_Text>();
+        _confirmButton = _panelContainer.GetComponentInChildren<Button>();
         _confirmButton.onClick.AddListener(LobbyButtonClick);
-    }
-
-    private void Start()
-    {
-        //// 획득한 경험치와 점수 출력
-        //_expText.text = ;
-        //_growthPointText.text = ;
     }
 
     // 로비 이동 버튼 클릭
     private void LobbyButtonClick()
     {
+        // 게임 클리어 확인
+        // if (!_battleManager._isClear)
+        // {
         // TODO : 김기운 : 추후에 마이씬 매니저 교체 예정
-        SceneManager.LoadScene("KGW_TestLobbyScene");
-
-        // 해당 UI 비활성화
-        //_panelContainer.SetActive(false);
-
-        _battleUIManager._defeatChapterUI.SetActive(false);
+        // SceneManager.LoadScene("KGW_TestLobbyScene");
+        GameManager.Instance.Scene.LoadSceneAsync(SceneName.SDW_LobbyScene, UIName.KGW_StageSelectUI);
+        OnUICloseRequested?.Invoke(UIName.DefeatChapterUI);
+        // }
     }
 }
