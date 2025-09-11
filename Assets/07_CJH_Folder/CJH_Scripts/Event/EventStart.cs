b76d04cd901e7d3aa3e2f3a0c5e304945a0712ef
jihyeog144 <jiheog144@gmail.com>
2025-09-11 17:26:20 +0900

    [Feat] 이벤트 버튼 및 UI 텍스트 세팅

--- Assets/07_CJH_Folder/CJH_Scripts/Event/EventStart.cs
+++ Assets/07_CJH_Folder/CJH_Scripts/Event/EventStart.cs
@@ -9,6 +9,7 @@ namespace CJH
     public class EventStart : MonoBehaviour
     {
         [Header("프리팹 내부 UI 연결")]
+        public TextMeshProUGUI eventTitleText; // 이벤트 종류 텍스트 (예: 긍정적 사건)
         public Image eventImage;
         public TextMeshProUGUI encounterText;
         public Transform buttonContainer;
@@ -29,6 +30,40 @@ namespace CJH
         public void Initialize(EncounterTable data)
         {
             _eventManager = FindObjectOfType<EventManager>();
+
+            // --- 이벤트 종류에 따라 제목 텍스트 설정 ---
+            if (eventTitleText != null)
+            {
+                switch (data.Type)
+                {
+                    case EncounterType.Money:
+                        eventTitleText.text = "긍정적 사건 발생!";
+                        break;
+                    case EncounterType.Relic:
+                        eventTitleText.text = "긍정적 사건 발생!";
+                        break;
+                    case EncounterType.RelicSel:
+                        eventTitleText.text = "긍정적 사건 발생!";
+                        break;
+                    case EncounterType.Luck:
+                        eventTitleText.text = "긍정적 사건 발생!";
+                        break;
+
+                    case EncounterType.MoneyFight:
+                        eventTitleText.text = "부정적 사건 발생!";
+                        break;
+                    case EncounterType.BadRelic:
+                        eventTitleText.text = "부정적 사건 발생!";
+                        break;
+                    case EncounterType.RelicDel:
+                        eventTitleText.text = "부정적 사건 발생!";
+                        break;
+                    default:
+                        eventTitleText.text = "사건 발생!";
+                        break;
+                }
+            }
+
             var mapping = encounterSprites.Find(m => m.typeEnum == data.Type);
             if (mapping != null && eventImage != null)
             {
@@ -55,7 +90,6 @@ namespace CJH
                 var buttonText = buttonObj.GetComponentInChildren<TextMeshProUGUI>();
                 if (buttonText != null && choiceIndex < data.ChoiceTexts.Count)
                 {
-                    // 만약 CSV에서 가져온 선택지 텍스트가 비어있다면, "확인"으로 표시합니다.
                     if (string.IsNullOrEmpty(data.ChoiceTexts[choiceIndex]) || data.ChoiceTexts[choiceIndex].ToLower() == "null")
                     {
                         buttonText.text = "확인";
@@ -79,6 +113,12 @@ namespace CJH
             {
                 resultPanel.SetActive(true);
                 resultText.text = data.EncounterExitText[choiceIndex];
+
+                // 결과창이 표시될 때 기존 UI 요소들을 비활성화합니다.
+                if (eventTitleText != null) eventTitleText.gameObject.SetActive(false);
+                eventImage.gameObject.SetActive(false);
+                encounterText.gameObject.SetActive(false);
+                buttonContainer.gameObject.SetActive(false);
             }
             else
             {
