6fb2115d70033c144691854740740ab7b3eda6ac
Rio_Song <rio.dw.song@gmail.com>
2025-09-18 11:03:55 +0900

    [Chore] 셰프의 흔적 CSV 추가 및 일부 매니저 폴더 이동
    * CSV와 관련된 Data Manager들은 DataManager로 폴더 이동

---
+++ Assets/06_SDW_Folder/Scripts/Struct/File/Contents/ChefsEchoDataFileData.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace SDW
+{
+    public struct ChefsEchoDataFileData
+    {
+        public int NodeID;
+        public NodeGrade Grade;
+        public string NodeName;
+        public NodeEnName EnName;
+        public string NodeDescription;
+        public int NodeCurrency;
+        public int NodeActiveCondition;
+        public NodeAbility Ability;
+        public float NodeAbilityValuePlus;
+        public float NodeAbilityValueMult;
+
+        public ChefsEchoDataFileData(string[] fields)
+        {
+            NodeID = int.Parse(fields[0]);
+            Grade = (NodeGrade)Enum.Parse(typeof(NodeGrade), fields[1]);
+            NodeName = fields[2];
+            EnName = (NodeEnName)Enum.Parse(typeof(NodeEnName), fields[3]);
+            NodeDescription = fields[4];
+            NodeCurrency = int.Parse(fields[5]);
+            NodeActiveCondition = int.Parse(fields[6]);
+            Ability = (NodeAbility)Enum.Parse(typeof(NodeAbility), fields[7]);
+            NodeAbilityValuePlus = float.Parse(fields[8]);
+            NodeAbilityValueMult = float.Parse(fields[9]);
+        }
+    }
+}
