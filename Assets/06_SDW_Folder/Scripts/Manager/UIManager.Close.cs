390793597ef872c8fc4ee55f4fb0d8b26dd49c30
Rio_Song <rio.dw.song@gmail.com>
2025-09-24 12:08:22 +0900

    [Feat] Gacha Result UI 연결
    * Gacha Result UI 연결

--- Assets/06_SDW_Folder/Scripts/Manager/UIManager.Close.cs
+++ Assets/06_SDW_Folder/Scripts/Manager/UIManager.Close.cs
@@ -338,8 +338,13 @@ namespace SDW
         private void DisconnectGachaConfirmUI(UIName uiName)
         {
             var gachaConfirmUI = _uiDic[uiName] as GachaConfirmUI;
+            var gachaResultUI = _uiDic[UIName.GachaResultUI] as GachaResultUI;
 
-            gachaConfirmUI.OnUIOpenRequested -= OpenPanel;
+            gachaConfirmUI.OnUIOpenRequested -= (uiName, isSingle) =>
+            {
+                gachaResultUI.SetGachaType(isSingle);
+                OpenPanel(uiName);
+            };
             gachaConfirmUI.OnUICloseRequested -= ClosePanel;
         }
 
