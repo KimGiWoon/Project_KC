903dfcf79e872fbaaf9c4ac3558de8ecaba8f9f3
jihyeog144 <jiheog144@gmail.com>
2025-09-27 17:13:16 +0900

    [Fix] 버그 픽스

--- Assets/07_CJH_Folder/CJH_Scripts/Organization/FinalTeamSlot.cs
+++ Assets/07_CJH_Folder/CJH_Scripts/Organization/FinalTeamSlot.cs
@@ -12,6 +12,34 @@ public class FinalTeamSlot : MonoBehaviour
 
     public CharacterDataSO characterData;
     private TeamFormationManager manager;
+    private TextMeshProUGUI orderNumberText;
+    private Image classIconImage;
+    [SerializeField] private Sprite[] classIconSprites;
+
+    private void Awake()
+    {
+        // OrderNumberText 라는 이름의 자식 오브젝트에서 TextMeshProUGUI 컴포넌트를 찾습니다.
+        Transform orderTextTransform = transform.Find("OrderNumberText");
+        if (orderTextTransform != null)
+        {
+            orderNumberText = orderTextTransform.GetComponent<TextMeshProUGUI>();
+        }
+        else
+        {
+            Debug.LogError("'OrderNumberText' 이름의 자식 오브젝트를 찾을 수 없습니다. 프리팹에서 이름을 확인해주세요.", gameObject);
+        }
+
+        // ClaasIcon 이라는 이름의 자식 오브젝트에서 Image 컴포넌트를 찾습니다.
+        Transform classIconTransform = transform.Find("ClaasIcon");
+        if (classIconTransform != null)
+        {
+            classIconImage = classIconTransform.GetComponent<Image>();
+        }
+        else
+        {
+            Debug.LogError("'ClaasIcon' 이름의 자식 오브젝트를 찾을 수 없습니다. 프리팹에서 이름을 확인해주세요.", gameObject);
+        }
+    }
 
     private void Start()
     {
@@ -20,7 +48,7 @@ public class FinalTeamSlot : MonoBehaviour
         if (characterButton) characterButton.onClick.AddListener(OnClick);
     }
 
-    public void DisplayCharacter(CharacterDataSO data)
+    public void DisplayCharacter(CharacterDataSO data, int index)
     {
         characterData = data;
 
@@ -28,12 +56,34 @@ public class FinalTeamSlot : MonoBehaviour
         characterImage.sprite = data._characterSprite;
         levelText.text = GameManager.Instance.CharacterData.CharEnNameLevel[data._chaBaseData.ChaEnName].ToString();
         characterButton.interactable = true;
+
+        // 편성 순서 표시 (1, 2, 3)
+        if (orderNumberText != null)
+        {
+            orderNumberText.text = (index + 1).ToString();
+            orderNumberText.gameObject.SetActive(true);
+        }
+
+        // 직업 아이콘 표시
+        if (classIconImage != null && classIconSprites != null && classIconSprites.Length > 0)
+        {
+            // CharacterDataSO의 _chaBaseData.ChaRole을 인덱스로 사용합니다.
+            int roleIndex = (int)data._chaBaseData.ChaRole;
+            if (roleIndex >= 0 && roleIndex < classIconSprites.Length)
+            {
+                classIconImage.sprite = classIconSprites[roleIndex];
+                classIconImage.gameObject.SetActive(true);
+            }
+        }
     }
 
     public void DisplayEmpty()
     {
         characterData = null;
         characterInfoGroup.SetActive(false);
+
+        if (orderNumberText != null) orderNumberText.gameObject.SetActive(false);
+        if (classIconImage != null) classIconImage.gameObject.SetActive(false);
     }
 
     private void OnClick()
